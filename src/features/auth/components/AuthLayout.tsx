f31826d03f74332e3ead21a903c51a43c32a65de
qp0kycHuk <serega990112@mail.ru>
2023-08-20 21:50:21 +0300

    add background

--- src/features/auth/components/AuthLayout.tsx
+++ src/features/auth/components/AuthLayout.tsx
@@ -1,15 +1,34 @@
 import { Suspense } from 'react'
 import classnames from 'classnames'
-import classes from '../assets/Auth.module.scss'
 import { LoggedInOutlet } from '@layouts/LoggedInOutlet'
 import { PagePreloader } from '@components/PagePreloader/PagePreloader'
+import classes from '../assets/Auth.module.scss'
 
 export function AuthLayout() {
   return (
-    <div className={classnames(classes.wrapper, 'flex items-center justify-center flex-grow')}>
-      <Suspense fallback={<PagePreloader />}>
-        <LoggedInOutlet />
-      </Suspense>
-    </div>
+    <>
+      <div className="fixed inset-0 pointer-events-none">
+        <video
+          autoPlay
+          muted
+          loop
+          controls={false}
+          className="absolute top-0 left-0 object-cover w-full h-full"
+        >
+          <source src="/media/auth-background.webm" type="video/webm" />
+          <source src="/media/auth-background.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div
+        className={classnames(
+          classes.wrapper,
+          'flex items-center justify-center flex-grow relative z-10'
+        )}
+      >
+        <Suspense fallback={<PagePreloader />}>
+          <LoggedInOutlet />
+        </Suspense>
+      </div>
+    </>
   )
 }
