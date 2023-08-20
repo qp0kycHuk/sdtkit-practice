30d88d06610212620841ccb908a2a0d700a5d983
qp0kycHuk <serega990112@mail.ru>
2023-08-20 21:50:35 +0300

    fix select first value

--- src/features/ui/components/Field/Select.tsx
+++ src/features/ui/components/Field/Select.tsx
@@ -10,18 +10,11 @@ export function Select({ children, fieldChildren, inputProps, ...props }: IField
     inputProps?.defaultValue === props.placeholder || !inputProps?.defaultValue
   )
 
-  useEffect(() => {
-    selectRef.current?.addEventListener('change', changeHandler)
-
-    return () => {
-      selectRef.current?.removeEventListener('change', changeHandler)
-    }
-  }, [selectRef])
-
-  function changeHandler(event: Event) {
+  function changeHandler(event: React.ChangeEvent<HTMLSelectElement>) {
     const target = event.target as HTMLSelectElement
     target.blur()
     setIsLabelShow(target?.value === props.placeholder)
+    inputProps?.onChange?.(event)
   }
 
   return (
@@ -35,19 +28,23 @@ export function Select({ children, fieldChildren, inputProps, ...props }: IField
           placeholder=""
           {...inputProps}
           defaultValue={inputProps?.defaultValue || props.placeholder}
+          onChange={changeHandler}
           className={classnames(
             'input w-full border-none focus:shadow-none appearance-none cursor-pointer',
 
             inputProps?.className
           )}
         >
+          <option value={props.placeholder} hidden disabled>
+            {props.placeholder}
+          </option>
           {children}
         </select>
       }
     >
       {fieldChildren}
       <div className="absolute top-0 right-0 flex items-center h-full pr-4 pointer-events-none">
-        <ToRightIcon className="text-lg opacity-40 transition-transform group-focus-within:-rotate-90" />
+        <ToRightIcon className="text-lg transition-transform opacity-40 group-focus-within:-rotate-90" />
       </div>
     </FieldWrapper>
   )
